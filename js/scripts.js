135ac8702f0d8935cf79e6ba9dcef3f9cf6909fe
semhar <semaria.eyzac@gmal.com>
2018-03-24 00:10:41 -0700

    almost done

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,47 @@
-function Contact(Name, Initial) {
-  this.firstName = Name;
-  this.initialDeposit = Initial;
-  this.totalbalance = [];
-}
-function Deposit(depositamount, withdrawal) {
-  this.depositamount = Deposit;
-  this.initialDeposit = Withdraw;
-}
+function Account(name, balance) {
+  this.name = name;
+  this.balance = balance;
+};
 
+Account.prototype.Adjust = function(){
+  return this.balance += (this.newDeposit) - (this.newWithdrawl);
+};
 
-
-
-
-
-
-
-
-
+function resetFields() {
+  $("#Deposit").val("");
+  $("#withdrawl").val("");
+};
 
 $(document).ready(function(){
   $("form#money").submit(function(event){
     event.preventDefault();
+    debugger;
     var inputtedFirstName = $("input#name").val();
-    var inputtedInitial = parseInt($("input#initial").val());
-    var newContact = new Contact (inputtedFirstName, inputtedInitial);
+    var inputtedInitialDeposit = parseInt($("input#initialDeposit").val());
+    initialDeposit = initialDeposit || 0;
+    var newAccount = new Account (inputtedFirstName, inputtedInitialDeposit);
+    $("#new-client").hide();
+    $("#manage").show()
     //needs change
-    $(".result").each(function() {
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
-      newContact.addresses.push(newAddress)
+    //alert(newAccount);
+    $("form#transaction").submit(function(){
+      event.preventDefault();
+      var deposit = $("input#Deposit").val();
+      var withdrawl = $("input#withdrawl").val();
+      deposit = deposit || 0;
+      withdrawl = withdrawl || 0;
+      newAccount.newDeposit = deposit;
+      newAccount.newWithdrawl = withdrawl;
+
+      newAccount.Adjust();
+
+      $("#account-balance").text(newAccount.balance);
+
+      resetFields();
     });
+
+      $("#account-name").text(newAccount.name);
+      $("#account-balance").text(newAccount.balance);
+
+    });
+  });
